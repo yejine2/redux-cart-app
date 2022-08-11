c4060180e7ba3a982f04196ad9ba4ac34d9d1937
yejine2 <yejine2@naver.com>
2022-08-11 23:13:56 +0900

    feat: Modify reder

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import "./index.css";
 import App from "./App";
 import store from "./store/index";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  // 리덕스 전역 설정
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   // 리덕스 전역 설정
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
