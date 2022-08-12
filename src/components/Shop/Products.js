e4d589461120c8e8ac104399d0ca61c04a8554a0
yejine2 <yejine2@naver.com>
2022-08-13 01:13:32 +0900

    feat: Modify dummy data

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -5,13 +5,13 @@ const DUMMY_PRODUCTS = [
   {
     id: "p1",
     price: 6,
-    name: "My First Book",
+    title: "My First Book",
     description: "The first book I ever wrote",
   },
   {
     id: "p2",
     price: 7,
-    name: "My Second Book",
+    title: "My Second Book",
     description: "The second book I ever wrote",
   },
 ];
